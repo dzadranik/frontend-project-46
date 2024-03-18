ce56c394c5cf13ca1e8d50475becf5768d955cfa
Виктория Короткова <v.korotkova@i-dgtl.ru>
2024-03-18 16:48:22 +0300

    step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,1 +1,15 @@
-console.log('gendiff');
+#!/usr/bin/env node
+// file: gendiff.js
+import { program } from 'commander';
+
+const command = () => {
+  console.log('Hello, World!');
+};
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action(command)
+  .parse(process.argv);
