adf6e2b0999541a843dcc5d0422eb270c90671d2
Виктория Короткова <v.korotkova@i-dgtl.ru>
2024-03-19 10:18:12 +0300

    step 6

--- src/gendiff.js
+++ src/gendiff.js
@@ -11,29 +11,33 @@ const gendiff = (format, conf1, conf2) => {
     config2 = yaml.load(conf2);
   }
 
-  const keys1 = Object.keys(config1).sort();
-  const keys2 = Object.keys(config2).sort();
-  const diff = ["{"];
-  for (const key of keys1) {
-    if (keys2.includes(key)) {
-      if (config1[key] === config2[key]) {
-        diff.push(`    ${key}: ${config1[key]}`);
+  if (conf1 && conf2) {
+    const keys1 = Object.keys(config1).sort();
+    const keys2 = Object.keys(config2).sort();
+    const diff = ["{"];
+    for (const key of keys1) {
+      if (keys2.includes(key)) {
+        if (config1[key] === config2[key]) {
+          diff.push(`    ${key}: ${config1[key]}`);
+        } else {
+          diff.push(`  - ${key}: ${config1[key]}`);
+          diff.push(`  + ${key}: ${config2[key]}`);
+        }
       } else {
         diff.push(`  - ${key}: ${config1[key]}`);
-        diff.push(`  + ${key}: ${config2[key]}`);
       }
-    } else {
-      diff.push(`  - ${key}: ${config1[key]}`);
     }
-  }
-  keys2
-    .filter(key => !keys1.includes(key))
-    .forEach(key => {
-      diff.push(`  + ${key}: ${config2[key]}`);
-    });
-  diff.push("}");
+    keys2
+      .filter(key => !keys1.includes(key))
+      .forEach(key => {
+        diff.push(`  + ${key}: ${config2[key]}`);
+      });
+    diff.push("}");
 
-  return diff.join("\n");
+    return diff.join("\n");
+  }
+
+  return null;
 };
 
 export default gendiff;
